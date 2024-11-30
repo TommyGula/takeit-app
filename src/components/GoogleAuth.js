8019f2a597968ac25a8236e8b22c270d148076fc
tomasgula <tomas.gula@nerdinteractive.com>
2024-11-30 17:02:48 -0300

    FINALLY FIXED THAT FUCKING GOOGLE SIGN IN ERROR

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,46 +2,46 @@ import React, { useEffect } from 'react';
 import { TouchableOpacity, StyleSheet, View, Text, Image } from 'react-native';
 import {
   GoogleSignin,
-  statusCodes,
 } from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
 import auth from '@react-native-firebase/auth';
 
-export default function GoogleAuth({text}) {
+export default function GoogleAuth({ text }) {
 
   useEffect(() => {
     GoogleSignin.configure({
-      webClientId: Config.GOOGLE_OAUTH_CLIENT_ID, 
-    });    
-  },[]);
+      webClientId: Config.GOOGLE_OAUTH_CLIENT_ID,
+    });
+  }, []);
 
   const onGoogleButtonPress = async () => {
-    console.log('Google Client ID ', Config.GOOGLE_OAUTH_CLIENT_ID)
     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
     const signInResult = await GoogleSignin.signIn();
-  
-    idToken = signInResult.data?.idToken;
-    if (!idToken) {
-      idToken = signInResult.idToken;
-    }
+
+    var idToken = signInResult.idToken;
+
     if (!idToken) {
       throw new Error('No ID token found');
     }
-  
+
     const googleCredential = auth.GoogleAuthProvider.credential(signInResult.data.token);
     return auth().signInWithCredential(googleCredential);
   };
 
-  return(
+  const signInWithGoogleToken = async () => {
+
+  };
+
+  return (
     <TouchableOpacity onPress={onGoogleButtonPress} style={googleStyles.button}>
-        <View style={googleStyles.iconContainer}>
-            <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png'}} style={googleStyles.icon} />
-        </View>
-        <View style={googleStyles.textContainer}>
-            <Text style={googleStyles.text}>{text}</Text>
-        </View>
+      <View style={googleStyles.iconContainer}>
+        <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png' }} style={googleStyles.icon} />
+      </View>
+      <View style={googleStyles.textContainer}>
+        <Text style={googleStyles.text}>{text}</Text>
+      </View>
     </TouchableOpacity>
-  )     
+  )
 };
 
 const googleStyles = StyleSheet.create({
@@ -73,6 +73,6 @@ const googleStyles = StyleSheet.create({
   text: {
     fontSize: 16,
     color: '#000', // Adjust text color as needed
-    fontFamily:'Quicksand-Bold',
+    fontFamily: 'Quicksand-Bold',
   },
 });
